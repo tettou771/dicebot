31cae0815c0797c985b78d105cf6e2309d479323
tettou771 <tettou771@gmail.com>
2021-01-10 22:28:32 +0900

    add capture.py

--- capture.py
+++ capture.py
@@ -1,16 +1,22 @@
+import time, random
 import picamera
 
-camera = picamera.PiCamera()
+def take:
+    camera = picamera.PiCamera()
 
-videoName = 'tmp/diceVideo.mp4'
-pictureName = 'tmp/dicePicture.jpg'
-duration = 5
+    name = str(time.time()) + '_' + str(random.randint(0,1000000))
+    videoPath = 'static/videos/' + name + '.mp4'
+    imagePath = 'static/videos/' + name + '.jpg'
+    duration = 5
 
-# record video and capture image
-camera.start_recording(videoName)
-sleep(duration)
-camera.stop_recording()
-camera.capture(pictureName)
+    # record video and capture image
+    camera.start_recording(videoPath)
+    sleep(duration)
+    camera.stop_recording()
+    camera.capture(imagePath)
 
-# send to nextcloud
+    return (videoPath, imagePath)
+if (name == '__main__'):
+    data = take()
+    print('video ' + data[0] + ' image ' + data[1])
 
--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def callback():
 
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text=event.message.text)
+            TextSendMessage(text = '稲造と' + event.message.text)
         )
 
     return 'OK'
